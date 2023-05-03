315a1e4b133c01751548c8c15f2f5f2eb7cae382
ÖMER YAĞMUR <omeryyagmur@gmail.com>
2023-05-03 12:28:41 +0300

    Add project files.

---
+++ Video Oynatıcı/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Video_Oynatıcı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblBaslik = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnVideoac = new System.Windows.Forms.Button();
+            this.btnDuraklat = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.btnOynat = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.wmpVideo = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblBaslik);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("SimSun-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1201, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblBaslik
+            // 
+            this.lblBaslik.AutoSize = true;
+            this.lblBaslik.Font = new System.Drawing.Font("SimSun-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBaslik.Location = new System.Drawing.Point(528, 23);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(71, 21);
+            this.lblBaslik.TabIndex = 0;
+            this.lblBaslik.Text = "Başlık";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.trackBar1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.BtnVideoac);
+            this.panel2.Controls.Add(this.btnDuraklat);
+            this.panel2.Controls.Add(this.btnDurdur);
+            this.panel2.Controls.Add(this.btnOynat);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Font = new System.Drawing.Font("SimSun-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(0, 565);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1201, 66);
+            this.panel2.TabIndex = 1;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(407, 14);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(140, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(616, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = ">>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnVideoac
+            // 
+            this.BtnVideoac.Location = new System.Drawing.Point(1088, 14);
+            this.BtnVideoac.Name = "BtnVideoac";
+            this.BtnVideoac.Size = new System.Drawing.Size(101, 39);
+            this.BtnVideoac.TabIndex = 3;
+            this.BtnVideoac.Text = "VİDEO AÇ";
+            this.BtnVideoac.UseVisualStyleBackColor = true;
+            this.BtnVideoac.Click += new System.EventHandler(this.BtnVideoac_Click);
+            // 
+            // btnDuraklat
+            // 
+            this.btnDuraklat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDuraklat.BackgroundImage")));
+            this.btnDuraklat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDuraklat.Location = new System.Drawing.Point(742, 14);
+            this.btnDuraklat.Name = "btnDuraklat";
+            this.btnDuraklat.Size = new System.Drawing.Size(57, 39);
+            this.btnDuraklat.TabIndex = 2;
+            this.btnDuraklat.UseVisualStyleBackColor = true;
+            this.btnDuraklat.Click += new System.EventHandler(this.btnDuraklat_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDurdur.BackgroundImage")));
+            this.btnDurdur.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDurdur.Location = new System.Drawing.Point(679, 14);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(57, 39);
+            this.btnDurdur.TabIndex = 1;
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // btnOynat
+            // 
+            this.btnOynat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnOynat.BackgroundImage")));
+            this.btnOynat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnOynat.Location = new System.Drawing.Point(553, 14);
+            this.btnOynat.Name = "btnOynat";
+            this.btnOynat.Size = new System.Drawing.Size(57, 39);
+            this.btnOynat.TabIndex = 0;
+            this.btnOynat.UseVisualStyleBackColor = true;
+            this.btnOynat.Click += new System.EventHandler(this.btnOynat_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.wmpVideo);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 66);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1201, 499);
+            this.panel3.TabIndex = 2;
+            // 
+            // wmpVideo
+            // 
+            this.wmpVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wmpVideo.Enabled = true;
+            this.wmpVideo.Location = new System.Drawing.Point(0, 0);
+            this.wmpVideo.Name = "wmpVideo";
+            this.wmpVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpVideo.OcxState")));
+            this.wmpVideo.Size = new System.Drawing.Size(1201, 499);
+            this.wmpVideo.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(1201, 631);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("SimSun-ExtB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button BtnVideoac;
+        private System.Windows.Forms.Button btnDuraklat;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Button btnOynat;
+        private System.Windows.Forms.Panel panel3;
+        private AxWMPLib.AxWindowsMediaPlayer wmpVideo;
+        private System.Windows.Forms.Label lblBaslik;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Video Oynatıcı/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Video_Oynatıcı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            wmpVideo.uiMode = "none";
+        }
+        string videooynatici,videoadi;
+
+        private void BtnVideoac_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog() { Multiselect = false, Filter = "MP4 OYNATICI|*.mp4|ALL FILE|" };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                videooynatici = openFileDialog.FileName;
+                videoadi = openFileDialog.SafeFileName;
+                wmpVideo.URL = videooynatici;
+                lblBaslik.Text = videoadi;
+            }
+        }
+
+        private void btnOynat_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.play();
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.stop();
+        }
+
+        private void btnDuraklat_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.pause();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.fastForward();
+        }
+
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            wmpVideo.settings.volume= trackBar1.Value;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
